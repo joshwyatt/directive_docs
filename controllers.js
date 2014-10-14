1811e0141c334a3475b940d123df79333390c8ea
joshWyatt <joshuawyatt123@gmail.com>
2014-10-14 16:32:26 -0700

    Add time ticker and friend lister directives

--- controllers.js
+++ controllers.js
@@ -11,6 +11,30 @@ function ControllerOne(){
   };
 }
 
+function ControllerTwo(){
+  var vm = this;
+
+  vm.war = {
+    question: "What is the opposite of war?",
+    answer: "Fucking is the opposite of war."
+  };
+
+  vm.peace = {
+    question: "What is the opposite of peace?",
+    answer: "There is no opposite to peace."
+  };
+}
+
+function ControllerThree(){
+  var vm = this;
+
+  vm.format = 'M/d/yy h:mm:ss a';
+
+  vm.friend = 'Liz';
+}
+
 angular.module('controllers', ['directives'])
 
 .controller('ControllerOne', ControllerOne)
+.controller('ControllerTwo', ControllerTwo)
+.controller('ControllerThree', ControllerThree)
--- directives.js
+++ directives.js
@@ -8,6 +8,111 @@ function myCustomer(){
   };
 }
 
+function myTestDirective(){
+  return {
+    restrict: 'E',
+    scope:{
+      dialogue: '='
+    },
+    templateUrl: 'my-test-iso.html'
+  };
+}
+
+function myCurrentTime($interval, dateFilter){
+
+  function link(scope, element, attrs){
+    
+    var format,
+        timeoutId;
+    
+    function updateTime(){
+      element.text(dateFilter(new Date(), format));
+    }
+
+    scope.$watch(attrs.myCurrentTime, function(value){
+      format = value;
+      updateTime();
+    });
+
+    element.on('$destroy', function(){
+      $interval.cancel(timeoutId);
+    });
+
+    timeoutId = $interval(function(){
+      updateTime(); //update DOM
+    }, 1000);
+  }
+
+  return {
+    link: link
+  };
+}
+
+function allMyFriends($interval){
+
+  function link(scope, element, attrs){
+    var ticker,
+        friend;
+
+    function showFriend(){
+      element.text(friend);
+    }
+
+    scope.$watch(attrs.allMyFriends, function(name){
+      friend = name;
+      showFriend();
+    })
+
+    ticker = $interval(function(){
+      showFriend();
+    }, 1000);
+
+    element.on('destroy', function(){
+      $interval.cancel(ticker);
+    });
+  }
+
+  return {
+    link: link
+  };
+}
+
+
+
 angular.module('directives', [])
 
 .directive('myCustomer', myCustomer)
+.directive('myTestDirective', myTestDirective)
+.directive('myCurrentTime', myCurrentTime)
+.directive('allMyFriends', allMyFriends)
+
+// function allMyFriends($interval){
+
+
+//   function link(scope, element, attrs){
+//     var friend,
+//         ticker
+
+//     function showFriend(){
+//       element.text(friend);
+//     }
+
+//     scope.$watch(attrs.allMyFriends, function(list){
+//       friend = list;
+//       showFriend();
+//     });
+
+//     element.on('$destroy', function(){
+//       $interval.cancel(ticker);
+//     });
+
+//     ticker = $interval(function(){
+//       showFriend();
+//     }, 1000);
+//   }
+
+
+//   return {
+//     link: link
+//   };
+// }
